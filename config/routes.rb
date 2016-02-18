ecc34a3f596642938faf45f3b7abf223e1d5fa7b
gagan <meesala.gagan@gmail.com>
2016-02-18 19:05:05 +0530

    Configured devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
 
   root :to => 'home#show'
 
+  devise_for :users, class_name: 'User',
+             :controllers =>
+                 {
+                     registrations: 'users/registrations',
+                     invitations: 'users/invitations',
+                     sessions: 'users/sessions',
+                 }
+
 end

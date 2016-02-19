25aa66b68d267f190fe094a376a88ebd65a0b79b
gagan <meesala.gagan@gmail.com>
2016-02-20 00:10:09 +0530

    Minor changes

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module Tms
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.enabled = true
-    config.sass.preferred_syntax = :sass
+    # config.assets.enabled = true
+    # config.sass.preferred_syntax = :sass
     config.exceptions_app = self.routes
 
     config.generators do |g|

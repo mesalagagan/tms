83b3d7b5692ea36721e5694abad4be41ed4ea21a
gagan <meesala.gagan@gmail.com>
2016-02-20 09:30:19 +0530

    configured email configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,29 +11,29 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  # config.action_doispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.0.5'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -62,7 +62,21 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+
+  # Default mailer from option
+  # config.action_mailer.default_options = {:from => 'tmsstaging.herokuapp.com'}
+
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'tmsstaging.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.mailgun.org',
+      :port => '587',
+      :authentication => :login,
+      :user_name => ENV['MAILGUN_USERNAME'],
+      :password => ENV['MAILGUN_PASSWORD'],
+      :domain => 'tmsstaging.herokuapp.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -77,22 +91,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.perform_deliveries = true
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.default_url_options = { host: 'tmsstaging.herokuapp.com' }
-
-  config.action_mailer.smtp_settings = {
-      :address => 'smtp.mailgun.org',
-      :port => '587',
-      :authentication => :plain,
-      :user_name => ENV['MAILGUN_USERNAME'],
-      :password => ENV['MAILGUN_PASSWORD'],
-      :domain => 'tmsstaging.herokuapp.com'
-  }
-
 end

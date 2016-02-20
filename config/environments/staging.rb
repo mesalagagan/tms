a17e83eea50f2863ef1513039c560d010de12f53
gagan <meesala.gagan@gmail.com>
2016-02-20 08:57:15 +0530

    Minor changes in email conf

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -77,19 +77,19 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.perform_deliveries = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
 
-  config.app_domain = 'http://tmsstaging.herokuapp.com'
-
-  config.action_mailer.default_url_options = { host: config.app_domain}
+  config.action_mailer.default_url_options = { host: 'tmsstaging.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
-      :domain               => 'http://tmsstaging.herokuapp.com',
+      :domain               => 'tmsstaging.herokuapp.com',
       :user_name            => 'emstestapp@gmail.com',
       :password             => 'testtest1',
       :authentication       => 'plain',
